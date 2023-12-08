b9c83737ebbfe82b32d71abf89378b02579d0908
Lucky Victory <luckyvictory54@gmail.com>
2023-12-08 17:07:05 +0000

    updated article view page, added markdown renderer for article

---
+++ frontend/src/types/shared.d.ts
@@ -0,0 +1,7 @@
+export type Article={
+    id:string;slug:string;
+title:string;content:string;image?:string;status?:ArticleStatus,intro?:string;createdAt:number|Date;updatedAt?:number|Date; author:{
+    name:string;avatar?:string
+}
+}
+export type ArticleStatus='published'|'draft'
